e4ea9824f2727d87af3ed0167907ab5dedce8b0f
Othi-Raja <othiraja5677@gmail.com>
2025-11-11 17:59:43 +0530

    feat: update trace logging and memory usage tracking in development environment
    fix: update import paths for routes in TypeScript environment
    
    chore: add @types/react-router-dom and @types/history to package dependencies
    
    chore: update package.json and package-lock.json with new type definitions
    
    refactor: comment out NavLink component implementation for future use
    
    fix: correct import paths for button components in various UI components
    
    fix: update import paths for toast and dialog components in UI
    
    fix: update firebase configuration to use environment variables
    
    delete: remove obsolete Supabase configuration file

--- src/app/integrations/firebaseConfig/config.js
+++ src/app/integrations/firebaseConfig/config.js
@@ -4,15 +4,16 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAZrXqVD7NVdYOVAzjDko-ONksRoMTc7ho",
-  authDomain: "portfolioformdata-4caae.firebaseapp.com",
-  databaseURL: "https://portfolioformdata-4caae-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "portfolioformdata-4caae",
-  storageBucket: "portfolioformdata-4caae.appspot.com",
-  messagingSenderId: "51070256017",
-  appId: "1:51070256017:web:9395f746a33189a57c1030",
-  measurementId: "G-PEYFMT3DE4"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
+
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
